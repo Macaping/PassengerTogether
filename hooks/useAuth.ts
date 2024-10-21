77a1df368d3dbee3e9aaae51af558ad8e868dcb8
Halozhan <juwonhwang0411@gmail.com>
2024-10-21 11:50:37 +0900

    기존 작업물을 expo로 옮기다

---
+++ hooks/useAuth.ts
@@ -0,0 +1,52 @@
+import { router } from "expo-router";
+import { signIn, signOut, signUp } from "../services/auth";
+import { useState } from "react";
+
+const useAuth = () => {
+    const [user, setUser] = useState(null);
+
+    const handleSignIn = async (email: string, password: string) => {
+        try {
+            const { data, error } = await signIn(email, password);
+            if (error) {
+                throw new Error(error.message);
+            }
+            setUser(data.user);
+            console.log(data.user);
+            console.log("로그인 성공!");
+            router.back();
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const handleSignUp = async (email: string, password: string) => {
+        try {
+            const { data, error } = await signUp(email, password);
+            if (error) {
+                throw new Error(error.message);
+            }
+            setUser(data.user);
+            console.log(data.user);
+            console.log("회원가입 성공!");
+            router.back();
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const handleSignOut = async () => {
+        try {
+            await signOut();
+            setUser(null);
+            console.log("로그아웃 성공!");
+            router.push('/LoginView');
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    return { user, handleSignIn, handleSignUp, handleSignOut };
+};
+
+export default useAuth;
