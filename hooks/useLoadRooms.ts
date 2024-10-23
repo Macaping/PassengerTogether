213c2b0280587170c72cc1b5ef6c92ea4a387399
Halozhan <juwonhwang0411@gmail.com>
2024-10-23 11:03:56 +0900

    useLoadRooms hook 예외 핸들링 수정

--- hooks/useLoadRooms.ts
+++ hooks/useLoadRooms.ts
@@ -1,35 +1,33 @@
 import { supabase } from "@/services/supabaseClient";
+import { PostgrestError } from "@supabase/supabase-js";
 import { useState, useEffect } from "react";
 
 const useLoadRooms = () => {
     const [loading, setLoading] = useState(false);
-    const [rooms, setRooms] = useState<any[]>([]);
+    const [rooms, setRooms] = useState<any[] | null>();
     const [error, setError] = useState<string | null>(null);
 
-    useEffect(() => {
-        const fetchRooms = async () => {
+    const fetchRooms = async () => {
+        try {
             setLoading(true);
-            try {
-                const { data, error } = await supabase
-                    .from('rooms')
-                    .select('*');
-                
-                if (error) {
-                    throw error;
-                }
+            let { data, error } = await supabase.from("rooms").select("*");
 
-                setRooms(data);
-            } catch (error) {
-                setError(error.message);
-            } finally {
-                setLoading(false);
+            if (error) {
+                throw error;
             }
-        };
+            setRooms(data);
+        } catch (error) {
+            setError((error as PostgrestError).message);
+        } finally {
+            setLoading(false);
+        }
+    };
 
+    useEffect(() => {
         fetchRooms();
     }, []);
 
-    return { loading: loading, rooms: rooms, error: error };
+    return { loading, rooms, error };
 };
 
 export default useLoadRooms;
