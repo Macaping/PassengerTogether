132fdb1bcfbb92ae71a53788801038ca9d5dd282
Kimagha <todrkr815@gmail.com>
2024-11-01 18:15:36 +0900

    index.jsx에서 react import 오류 수정

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -1,7 +1,7 @@
 import DateTimePicker from '@react-native-community/datetimepicker';
 import * as Location from 'expo-location';
 import { Link } from 'expo-router';
-import React, { default as React, default as React, useEffect, useRef, useState } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import { FlatList, Modal, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import MapView, { Marker, Polyline } from 'react-native-maps';
 import { StatusBar } from 'react-native-web';
