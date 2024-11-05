0759328ed856afa1abbd5141a4556f2afd9e8b29
Halozhan <juwonhwang0411@gmail.com>
2024-11-06 00:46:55 +0900

    supabase 인증 세션 관리

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -29,6 +29,32 @@ export default function RootLayout() {
     return null;
   }
 
+  //#region Supabase Auth
+  const [session, setSession] = useState<Session | null>(null)
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
+
+  // Tells Supabase Auth to continuously refresh the session automatically if
+  // the app is in the foreground. When this is added, you will continue to receive
+  // `onAuthStateChange` events with the `TOKEN_REFRESHED` or `SIGNED_OUT` event
+  // if the user's session is terminated. This should only be registered once.
+  AppState.addEventListener('change', (state) => {
+    if (state === 'active') {
+      supabase.auth.startAutoRefresh()
+    } else {
+      supabase.auth.stopAutoRefresh()
+    }
+  })
+  //#endregion
+
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
