ba6120a27ce562d2335c451f98fe8b97c663beab
Halozhan <juwonhwang0411@gmail.com>
2024-11-06 03:10:27 +0900

    오류나는 부분 주석처리

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -30,29 +30,29 @@ export default function RootLayout() {
   }
 
   //#region Supabase Auth
-  const [session, setSession] = useState<Session | null>(null)
+  // const [session, setSession] = useState<Session | null>(null)
 
-  useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session)
-    })
+  // useEffect(() => {
+  //   supabase.auth.getSession().then(({ data: { session } }) => {
+  //     setSession(session)
+  //   })
 
-    supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session)
-    })
-  }, [])
+  //   supabase.auth.onAuthStateChange((_event, session) => {
+  //     setSession(session)
+  //   })
+  // }, [])
 
-  // Tells Supabase Auth to continuously refresh the session automatically if
-  // the app is in the foreground. When this is added, you will continue to receive
-  // `onAuthStateChange` events with the `TOKEN_REFRESHED` or `SIGNED_OUT` event
-  // if the user's session is terminated. This should only be registered once.
-  AppState.addEventListener('change', (state) => {
-    if (state === 'active') {
-      supabase.auth.startAutoRefresh()
-    } else {
-      supabase.auth.stopAutoRefresh()
-    }
-  })
+  // // Tells Supabase Auth to continuously refresh the session automatically if
+  // // the app is in the foreground. When this is added, you will continue to receive
+  // // `onAuthStateChange` events with the `TOKEN_REFRESHED` or `SIGNED_OUT` event
+  // // if the user's session is terminated. This should only be registered once.
+  // AppState.addEventListener('change', (state) => {
+  //   if (state === 'active') {
+  //     supabase.auth.startAutoRefresh()
+  //   } else {
+  //     supabase.auth.stopAutoRefresh()
+  //   }
+  // })
   //#endregion
 
   return (
