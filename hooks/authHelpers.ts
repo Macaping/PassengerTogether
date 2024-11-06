fbe962a17172d9c08d7dcb6433fff9e6e7ea5036
Halozhan <juwonhwang0411@gmail.com>
2024-11-06 22:13:41 +0900

    authHelpers import 수정

--- hooks/authHelpers.ts
+++ hooks/authHelpers.ts
@@ -1,4 +1,4 @@
-import { supabase } from '../services/supabaseClient';
+import { supabase } from '@/lib/supabase';
 
 export async function getCurrentUserId() {
     const { data, error } = await supabase.auth.getUser();
