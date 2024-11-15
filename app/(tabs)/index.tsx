aa181d560145b024b66816a22b2975f79799d238
Halozhan <juwonhwang0411@gmail.com>
2024-11-15 21:30:02 +0900

    todo setShowDatePicker, setShowTimePicker 오류 발생

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,69 +3,91 @@ import { MapWidget } from "@/components/home/MapWidget";
 import { 방_탐색 } from "@/components/home/방_탐색";
 import { 방장_하기 } from "@/components/home/방장_하기";
 import { DateTimePickerEvent } from "@react-native-community/datetimepicker";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Dimensions, StyleSheet, View } from "react-native";
-import MapView from "react-native-maps";
+import { LatLng } from "react-native-maps";
 
 const { width } = Dimensions.get("window"); //Dimensions 이용
 
-export default function HomeView() {
-  const locations = ["천안역", "천안아산역", "선문대", "탕정역", "두정동 롯데"];
-  interface Coordinates {
-    latitude: number;
-    longitude: number;
-  }
-  const coordinates: { [key: string]: Coordinates } = {
-    천안역: { latitude: 36.8089885, longitude: 127.148933 },
-    천안아산역: { latitude: 36.7946071, longitude: 127.1045608 },
-    선문대: { latitude: 36.7989764, longitude: 127.0750025 },
-    탕정역: { latitude: 36.78827, longitude: 127.084638 },
-    "두정동 롯데": { latitude: 36.8261834, longitude: 127.1399744 },
-  };
+const coordinates: { [key: string]: LatLng } = {
+  천안역: { latitude: 36.8089885, longitude: 127.148933 },
+  천안아산역: { latitude: 36.7946071, longitude: 127.1045608 },
+  선문대: { latitude: 36.7989764, longitude: 127.0750025 },
+  탕정역: { latitude: 36.78827, longitude: 127.084638 },
+  "두정동 롯데": { latitude: 36.8261834, longitude: 127.1399744 },
+};
+const locations = ["천안역", "천안아산역", "선문대", "탕정역", "두정동 롯데"];
 
-  const [selectedDeparture, setSelectedDeparture] = useState(locations[0]);
-  const [selectedDestination, setSelectedDestination] = useState(locations[1]);
+export default function HomeView() {
+  // interface Coordinates {
+  //   latitude: number;
+  //   longitude: number;
+  // }
+
+  // 출발지, 도착지
+  const [departure, setDeparture] = useState<LatLng>(coordinates["천안역"]);
+  const [destination, setDestination] = useState<LatLng>(
+    coordinates["천안아산역"],
+  );
+  const [selectedDeparture, setSelectedDeparture] = useState<string | null>(
+    "천안역",
+  );
+  const [selectedDestination, setSelectedDestination] = useState<string | null>(
+    "천안아산역",
+  );
+  useEffect(() => {
+    if (selectedDeparture) {
+      setDeparture(coordinates[selectedDeparture]);
+    }
+    if (selectedDestination) {
+      setDestination(coordinates[selectedDestination]);
+    }
+  }, [selectedDeparture, selectedDestination]);
+
+  // const [selectedDeparture, setSelectedDeparture] = useState(locations[0]);
+  // const [selectedDestination, setSelectedDestination] = useState(locations[1]);
   const [date, setDate] = useState(new Date());
   const [showDatePicker, setShowDatePicker] = useState(false);
   const [showTimePicker, setShowTimePicker] = useState(false);
   const [modalVisible, setModalVisible] = useState(false);
   const [changingLocationType, setChangingLocationType] = useState("departure");
 
-  const [route, setRoute] = useState<{ latitude: number; longitude: number }[]>(
-    [],
-  );
+  // const [route, setRoute] = useState<{ latitude: number; longitude: number }[]>(
+  //   [],
+  // );
+  // 경로 정보
   const [distance, setDistance] = useState<number | null>(null);
   const [duration, setDuration] = useState<number | null>(null);
 
-  useEffect(() => {
-    fetchRouteData();
-  }, [selectedDeparture, selectedDestination]);
+  // useEffect(() => {
+  //   fetchRouteData();
+  // }, [selectedDeparture, selectedDestination]);
 
-  const fetchRouteData = async () => {
-    const departureCoord = coordinates[selectedDeparture];
-    const destinationCoord = coordinates[selectedDestination];
+  // const fetchRouteData = async () => {
+  //   const departureCoord = coordinates[selectedDeparture];
+  //   const destinationCoord = coordinates[selectedDestination];
 
-    const MAP_KEY = process.env.EXPO_PUBLIC_MAP_KEY || ""; //길찾기 api 키 가져옴
-    const url = `https://api.openrouteservice.org/v2/directions/driving-car?api_key=${MAP_KEY}&start=${departureCoord.longitude},${departureCoord.latitude}&end=${destinationCoord.longitude},${destinationCoord.latitude}`;
+  //   const MAP_KEY = process.env.EXPO_PUBLIC_MAP_KEY || ""; //길찾기 api 키 가져옴
+  //   const url = `https://api.openrouteservice.org/v2/directions/driving-car?api_key=${MAP_KEY}&start=${departureCoord.longitude},${departureCoord.latitude}&end=${destinationCoord.longitude},${destinationCoord.latitude}`;
 
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-      const geometry = data.features[0].geometry.coordinates;
-      const polylineCoords: { latitude: number; longitude: number }[] =
-        geometry.map((coord: [number, number]) => ({
-          latitude: coord[1],
-          longitude: coord[0],
-        }));
-      setRoute(polylineCoords);
+  //   try {
+  //     const response = await fetch(url);
+  //     const data = await response.json();
+  //     // const geometry = data.features[0].geometry.coordinates;
+  //     // const polylineCoords: { latitude: number; longitude: number }[] =
+  //     //   geometry.map((coord: [number, number]) => ({
+  //     //     latitude: coord[1],
+  //     //     longitude: coord[0],
+  //     //   }));
+  //     // setRoute(polylineCoords);
 
-      const summary = data.features[0].properties.summary;
-      setDistance(summary.distance / 1000); // distance in km
-      setDuration(summary.duration / 60); // duration in minutes
-    } catch (error) {
-      console.error("Error fetching route data:", error);
-    }
-  };
+  //     const summary = data.features[0].properties.summary;
+  //     setDistance(summary.distance / 1000); // distance in km
+  //     setDuration(summary.duration / 60); // duration in minutes
+  //   } catch (error) {
+  //     console.error("Error fetching route data:", error);
+  //   }
+  // };
 
   const handleDateChange = (
     _event: DateTimePickerEvent,
@@ -101,37 +123,42 @@ export default function HomeView() {
     setModalVisible(true);
   };
 
-  const mapRef = useRef<MapView | null>(null);
+  // const mapRef = useRef<MapView | null>(null);
 
   const handleLocationSelect = (location: string) => {
     if (changingLocationType === "departure") {
       setSelectedDeparture(location);
+      setDeparture(coordinates[location]);
     } else {
       setSelectedDestination(location);
+      setDestination(coordinates[location]);
     }
     setModalVisible(false);
 
-    const selectedCoordinates: Coordinates = coordinates[location];
-    mapRef.current?.animateToRegion(
-      {
-        latitude: selectedCoordinates.latitude,
-        longitude: selectedCoordinates.longitude,
-        latitudeDelta: 0.01,
-        longitudeDelta: 0.01,
-      },
-      1000, // 애니메이션 지속 시간 (1초)
-    );
+    // const selectedCoordinates: Coordinates = coordinates[location];
+    // mapRef.current?.animateToRegion(
+    //   {
+    //     latitude: selectedCoordinates.latitude,
+    //     longitude: selectedCoordinates.longitude,
+    //     latitudeDelta: 0.01,
+    //     longitudeDelta: 0.01,
+    //   },
+    //   1000, // 애니메이션 지속 시간 (1초)
+    // );
   };
 
   return (
     <View style={styles.container}>
-      <MapWidget
-        coordinates={coordinates}
-        selectedDeparture={selectedDeparture}
-        selectedDestination={selectedDestination}
-        route={route}
-        mapRef={mapRef}
-      />
+      <View style={styles.map}>
+        <MapWidget
+          departure={departure}
+          setDeparture={setDeparture}
+          destination={destination}
+          setDestination={setDestination}
+          setDistance={setDistance}
+          setDuration={setDuration}
+        />
+      </View>
 
       <View style={styles.interface}>
         {/* 상호작용 UI */}
@@ -178,6 +205,16 @@ const styles = StyleSheet.create({
     backgroundColor: "#FFFFFF",
     // 맨 아래로 정렬
     justifyContent: "flex-end",
+  },
+  map: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    height: "75%",
+    zIndex: 0,
+    borderWidth: 1,
+    borderColor: "#ccc",
   },
   interface: {
     padding: "5%",
--- components/home/MapWidget.tsx
+++ components/home/MapWidget.tsx
@@ -1,84 +1,152 @@
 import * as Location from "expo-location";
 import { useEffect, useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
-import MapView, { Marker, Polyline } from "react-native-maps";
+import { StyleSheet } from "react-native";
+import MapView, { LatLng, Marker, Polyline, Region } from "react-native-maps";
+
+interface MapWidgetProps {
+  departure: LatLng | null;
+  setDeparture: React.Dispatch<React.SetStateAction<LatLng>>;
+  destination: LatLng | null;
+  setDestination: React.Dispatch<React.SetStateAction<LatLng>>;
+}
+
+export function fetchRouteData() {}
 
 export function MapWidget({
-  coordinates,
-  selectedDeparture,
-  selectedDestination,
-  route,
-  mapRef,
-}: {
-  coordinates: { [key: string]: { latitude: number; longitude: number } };
-  selectedDeparture: string;
-  selectedDestination: string;
-  route: { latitude: number; longitude: number }[] | null;
-  mapRef: React.RefObject<MapView>;
+  departure,
+  setDeparture,
+  destination,
+  setDestination,
+  setDistance,
+  setDuration,
+}: MapWidgetProps & {
+  setDistance: React.Dispatch<React.SetStateAction<number | null>>;
+  setDuration: React.Dispatch<React.SetStateAction<number | null>>;
 }) {
+  // 위치 정보
   const [location, setLocation] =
     useState<Location.LocationObjectCoords | null>(null);
-  const [errorMsg, setErrorMsg] = useState<string | null>(null);
 
+  // 위치 정보를 불러옵니다. 오류가 발생하면 에러 메시지를 설정합니다.
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
   useEffect(() => {
-    (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== "granted") {
-        setErrorMsg("위치 권한이 거부되었습니다.");
-        return;
-      }
-      let currentLocation = await Location.getCurrentPositionAsync({});
-      if (currentLocation && currentLocation.coords) {
-        setLocation(currentLocation.coords);
-      } else {
-        setErrorMsg("위치를 가져오는 데 실패했습니다.");
-      }
-    })();
+    Location.requestForegroundPermissionsAsync()
+      .then((value) => {
+        if (!value.status) throw new Error("위치 권한이 거부되었습니다.");
+      })
+      .then(() => {
+        Location.getCurrentPositionAsync({})
+          .then((value) => {
+            setLocation(value.coords);
+          })
+          .catch((error: Error) => {
+            throw error;
+          });
+      })
+      .catch((error: Error) => {
+        setErrorMsg(error.message);
+      });
   }, []);
 
+  // 상태관리에 필요한 것들
+
+  // 출발지
+  // const [departure, setDeparture] = useState<LatLng | null>(null);
+  // 도착지
+  // const [destination, setDestination] = useState<LatLng | null>(null);
+
+  const initialRegion: Region = {
+    latitude: location?.latitude || 36.79932,
+    longitude: location?.longitude || 127.074874,
+    latitudeDelta: location ? 0.025 : 0.1,
+    longitudeDelta: location ? 0.025 : 0.1,
+  };
+
+  // 길찾기
+  const MAP_KEY = process.env.EXPO_PUBLIC_MAP_KEY || ""; //길찾기 api 키 가져옴
+  const [route, setRoute] = useState<LatLng[]>([]);
+
+  useEffect(() => {
+    if (departure && destination) {
+      const url = `https://api.openrouteservice.org/v2/directions/driving-car?api_key=${MAP_KEY}&start=${departure?.longitude},${departure.latitude}&end=${destination.longitude},${destination.latitude}`;
+      fetch(url)
+        // json으로 변환
+        .then((value) => value.json())
+        // geometry를 coordinates로 변환
+        .then((value) => {
+          const geometry = value.features[0].geometry.coordinates;
+          const polylineCoords = geometry.map((coord: [number, number]) => ({
+            latitude: coord[1],
+            longitude: coord[0],
+          }));
+          // route에 저장
+          setRoute(polylineCoords);
+
+          const summary = value.features[0].properties.summary;
+          setDistance(summary.distance / 1000); // distance in km
+          setDuration(summary.duration / 60); // duration in minutes
+        })
+        .catch((error) => console.error("Error fetching route data:", error));
+    }
+    fetchRouteData();
+  }, [departure, destination]);
+
   return (
-    <View style={styles.mapInfoBox}>
-      {location ? (
-        <MapView
-          ref={mapRef} // 지도 참조 추가
-          style={styles.map}
-          initialRegion={{
-            latitude: location.latitude,
-            longitude: location.longitude,
-            latitudeDelta: 0.05,
-            longitudeDelta: 0.05,
-          }}
-        >
-          <Marker
-            coordinate={{
-              latitude: coordinates[selectedDeparture].latitude,
-              longitude: coordinates[selectedDeparture].longitude,
-            }}
-            title="출발지"
-          />
-          <Marker
-            coordinate={{
-              latitude: coordinates[selectedDestination].latitude,
-              longitude: coordinates[selectedDestination].longitude,
-            }}
-            title="도착지"
-          />
-          {route && (
-            <Polyline
-              coordinates={route}
-              strokeColor="#6B59CC"
-              strokeWidth={4}
-            />
-          )}
-        </MapView>
-      ) : (
-        <View style={styles.loadingContainer}>
-          <Text>위치를 불러오는 중...</Text>
-          {errorMsg ? <Text>{errorMsg}</Text> : null}
-        </View>
+    <MapView
+      style={styles.map}
+      initialRegion={initialRegion}
+      showsUserLocation={true}
+    >
+      {departure && <Marker coordinate={departure} title="출발지" />}
+      {destination && <Marker coordinate={destination} title="도착지" />}
+      {route && (
+        <Polyline coordinates={route} strokeColor="#6B59CC" strokeWidth={4} />
       )}
-    </View>
+    </MapView>
   );
+  // return (
+  //   <View style={styles.mapInfoBox}>
+  //     {location ? (
+  //       <MapView
+  //         ref={mapRef} // 지도 참조 추가
+  //         style={styles.map}
+  //         initialRegion={{
+  //           latitude: location.latitude,
+  //           longitude: location.longitude,
+  //           latitudeDelta: 0.05,
+  //           longitudeDelta: 0.05,
+  //         }}
+  //       >
+  //         <Marker
+  //           coordinate={{
+  //             latitude: coordinates[selectedDeparture].latitude,
+  //             longitude: coordinates[selectedDeparture].longitude,
+  //           }}
+  //           title="출발지"
+  //         />
+  //         <Marker
+  //           coordinate={{
+  //             latitude: coordinates[selectedDestination].latitude,
+  //             longitude: coordinates[selectedDestination].longitude,
+  //           }}
+  //           title="도착지"
+  //         />
+  //         {route && (
+  //           <Polyline
+  //             coordinates={route}
+  //             strokeColor="#6B59CC"
+  //             strokeWidth={4}
+  //           />
+  //         )}
+  //       </MapView>
+  //     ) : (
+  //       <View style={styles.loadingContainer}>
+  //         <Text>위치를 불러오는 중...</Text>
+  //         {errorMsg ? <Text>{errorMsg}</Text> : null}
+  //       </View>
+  //     )}
+  //   </View>
+  // );
 }
 
 const styles = StyleSheet.create({
@@ -93,7 +161,8 @@ const styles = StyleSheet.create({
     borderColor: "#ccc",
   },
   map: {
-    ...StyleSheet.absoluteFillObject,
+    width: "100%",
+    height: "100%",
   },
   loadingContainer: {
     flex: 1,
