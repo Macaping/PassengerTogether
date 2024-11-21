85e262ad5ec7d79b3029b4c4e23d4f7270ef6b96
Halozhan <juwonhwang0411@gmail.com>
2024-11-21 20:25:34 +0900

    useUserData 훅에서 불필요한 주석 제거 및 코드 정리

--- hooks/useUserData.ts
+++ hooks/useUserData.ts
@@ -8,53 +8,6 @@ type UserData = Database["public"]["Tables"]["users"]["Row"];
 
 export function useUserData() {
   // const { user } = useUser();
-  // const [userData, setUserData] = useState<UserData | null>(null);
-
-  // useEffect(() => {
-  //   if (!user?.id) {
-  //     setUserData(null);
-  //   } else {
-  //     // 초기화
-  //     supabase
-  //       .schema("public")
-  //       .from("users")
-  //       .select("*")
-  //       .eq("user_id", user.id)
-  //       .single()
-  //       .then((value) => {
-  //         setUserData(value.data);
-  //       });
-
-  //     // 실시간 구독
-  //     const subscription = supabase
-  //       .channel("userData")
-  //       .on(
-  //         "postgres_changes",
-  //         {
-  //           event: "*",
-  //           schema: "public",
-  //           table: "users",
-  //           filter: "user_id=eq." + user.id,
-  //         },
-  //         (payload) => {
-  //           setUserData(payload.new as UserData);
-  //           supabase.getChannels().forEach((channel) => {
-  //             console.log("useUserData에서 발생. 구독 중인 채널:", channel.subTopic);
-  //           });
-  //         },
-  //       )
-  //       .subscribe();
-  //   }
-  //   return () => {
-  //     // 이전 구독 해제
-  //     supabase.getChannels().forEach((channel) => {
-  //       if (channel.subTopic === "userData") {
-  //         channel.unsubscribe();
-  //       }
-  //     });
-  //   };
-  // }, [user]);
-
   // 유저 계정 가져오기
   const [user, setUser] = useState<User | null>(null);
   useEffect(() => {
@@ -63,13 +16,15 @@ export function useUserData() {
     });
   }, []);
 
-  // 유저 정보 가져오기
   const [userData, setUserData] = useState<UserData | null>(null);
+
   useEffect(() => {
-    if (!user) {
+    if (!user?.id) {
       setUserData(null);
     } else {
+      // 초기화
       supabase
+        .schema("public")
         .from("users")
         .select("*")
         .eq("user_id", user.id)
@@ -77,27 +32,39 @@ export function useUserData() {
         .then((value) => {
           setUserData(value.data);
         });
-    }
-  }, [user]);
 
-  // 유저 정보 실시간 업데이트
-  if (user?.id) {
-    supabase
-      .channel("userData")
-      .on(
-        "postgres_changes",
-        {
-          event: "*",
-          schema: "public",
-          table: "users",
-          filter: `user_id=eq.${user?.id}`,
-        },
-        (payload) => {
-          setUserData(payload.new as UserData);
-        },
-      )
-      .subscribe();
-  }
+      // 실시간 구독
+      const subscription = supabase
+        .channel("userData")
+        .on(
+          "postgres_changes",
+          {
+            event: "*",
+            schema: "public",
+            table: "users",
+            filter: `user_id=eq.${user.id}`,
+          },
+          (payload) => {
+            setUserData(payload.new as UserData);
+            supabase.getChannels().forEach((channel) => {
+              console.log(
+                "useUserData에서 발생. 구독 중인 채널:",
+                channel.subTopic,
+              );
+            });
+          },
+        )
+        .subscribe();
+    }
+    return () => {
+      // 이전 구독 해제
+      supabase.getChannels().forEach((channel) => {
+        if (channel.subTopic === "userData") {
+          channel.unsubscribe();
+        }
+      });
+    };
+  }, [user]);
 
   return { userData, setUserData };
 }
