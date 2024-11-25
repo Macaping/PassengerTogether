a756a2694715a683f5e5e331341077f78958719d
Moona <moonapark1126@gmail.com>
2024-11-25 10:51:09 +0900

    동승자 컴포넌트에서 TouchableOpacity의 onPress 핸들러를 스타일 속성으로 이동

--- components/my_party/동승자.tsx
+++ components/my_party/동승자.tsx
@@ -6,13 +6,11 @@ import { useRouter } from "expo-router";
 export default function 동승자() {
   const router = useRouter();
   return (
-    <TouchableOpacity style={styles.button}>
-      <Ionicons
-        name="people-outline"
-        size={32}
-        color="#666666"
-        onPress={() => router.push("/CheckPassenger")}
-      />
+    <TouchableOpacity
+      style={styles.button}
+      onPress={() => router.push("/CheckPassenger")}
+    >
+      <Ionicons name="people-outline" size={32} color="#666666" />
       <Text style={styles.buttonText}>동승자</Text>
     </TouchableOpacity>
   );
