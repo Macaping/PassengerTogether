c1df4df9e86582fb337cc287def9694e9db14a18
KangGuYong <rivenine123@naver.com>
2024-11-27 10:41:30 +0900

    방을 생성할 때 유저의 옷 정보 저장

--- services/create_room.ts
+++ services/create_room.ts
@@ -1,4 +1,5 @@
 import { supabase } from "@/lib/supabase";
+import { User } from "./user";
 
 export async function CreateRoom({
   departure_time,
@@ -7,6 +8,7 @@ export async function CreateRoom({
   users = [],
   details,
   room_name,
+  meetingPlace,
 }: {
   departure_time: string;
   origin: string;
@@ -14,15 +16,19 @@ export async function CreateRoom({
   users: string[];
   details: string;
   room_name: string;
+  meetingPlace: string;
 }) {
   var room = {
     departure_time: departure_time,
     origin: origin,
     destination: destination,
     users: users,
-    details: details,
+    details: meetingPlace,
     room_name: room_name,
   };
+
+  const user = await User();
+  if (!user?.id) throw Error("사용자 정보가 없습니다.");
 
   try {
     // 방을 생성하고 생성된 방 데이터를 반환
@@ -32,11 +38,20 @@ export async function CreateRoom({
       .select();
 
     if (error) throw error;
+
+    // meetingPlace 값을 clothes 컬럼에 업데이트
+    const { error: updateError } = await supabase
+      .from("users")
+      .update({ clothes: details }) // meetingPlace 값을 clothes로 설정
+      .eq("user_id", user.id); // `uuid` 컬럼으로 조건 변경
+
+    if (updateError) throw updateError;
+
     // 성공적으로 생성된 방 데이터 반환
     return data.at(0);
   } catch (error) {
     // 오류
-    console.error("Failed to create room:", error);
+    console.error("Failed to create room or update clothes:", error);
     throw error;
   }
 }
